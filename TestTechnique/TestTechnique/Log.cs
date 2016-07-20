df7f90cb7b8d5b249710a815afcd4f435ec602b0
NicolasLeMerdy <nicolaslemerdy@gmail.com>
2016-07-20 02:54:34 +0200

    ADD Project

---
+++ TestTechnique/TestTechnique/Log.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTechnique {
+    public class Log {
+
+        private static ILog _logger;
+        private static ILog Logger {
+            get {
+                if (_logger == null) {
+                    _logger = LogManager.GetLogger(typeof(Log));
+                    log4net.Config.XmlConfigurator.Configure();
+                }
+                return _logger;
+            }
+        }
+
+        public static void LogError(string msg, Exception ex) {
+            Logger.Error(msg, ex);
+        }
+    }
+}
---
+++ TestTechnique/TestTechnique/WebMethod.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace TestTechnique {
+    public class WebMethod {
+
+        public double FibonacciSequenceCalcul(double n) {
+            try {
+
+                if (n <= 0 || n > 100) {
+                    return -1;
+                }
+
+                double primValue = 0;
+                double secondValue = 1;
+
+                for (var i = 1; i <= n; i++) {
+                    var concatValue = primValue;
+                    primValue = secondValue;
+                    secondValue = concatValue + secondValue;
+                }
+
+                return primValue;
+            }
+            catch (Exception ex) {
+                Log.LogError("Fibonacci", ex);
+                return -1;
+            }
+        }
+
+        public string XmlToJonReader(string xml) {
+            try {
+
+                XmlDocument Docxml = new XmlDocument();
+                Docxml.LoadXml(xml);
+                string jsonText = JsonConvert.SerializeXmlNode(Docxml);
+                return jsonText;
+
+            }
+            catch (Exception ex) {
+                Log.LogError("XmlToJson", ex);
+                return "Bad Xml format";
+            }
+
+        }
+
+    }
+}
